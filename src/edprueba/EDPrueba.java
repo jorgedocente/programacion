51b3b9ae2f6b4b11a40de74b9e67445f331fa8a1
jbautista <jbautista@SERVIDOR-CFGS1.ciclos.local>
2020-02-19 13:39:25 +0100

    commit inicial

--- src/edprueba/EDPrueba.java
+++ src/edprueba/EDPrueba.java
@@ -16,6 +16,7 @@ public class EDPrueba {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Hola Clase");
     }
     
 }
